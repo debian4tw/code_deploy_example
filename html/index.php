eae86752f5e855854c7041a0ccf5fbf4b0a7dbad
Matt <matt@MMB.local>
2017-10-10 01:36:07 -0300

    Added docker stuff

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<html>
+<head></head>
+<body>
+  <h1>Index |_|</h1>
+<?php
+
+$servername = "mysql:3306";
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    echo("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>  
+  <?php echo phpinfo();?>
+</body>
+</html>
