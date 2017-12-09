7c89306485e3293761b71cc488d2c112f7b753f0
Matt <matt@MMB.local>
2017-12-09 03:22:19 -0300

    test

--- app/server.js
+++ app/server.js
@@ -2,6 +2,25 @@
 
 const express = require('express');
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'mysql',
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASSWORD,
+  database : 'mysql'
+});
+
+connection.connect();
+var solution = "";
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  solution = results[0].solution
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -9,8 +28,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world 33\n');
+  res.send('Hello world 33 test\n'+ solution);
 });
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
